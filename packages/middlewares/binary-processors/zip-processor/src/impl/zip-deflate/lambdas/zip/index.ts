8d8b8c22854d306e909991577a51717c4c248c8e
HQarroum <qarroumh@amazon.lu>
2024-04-11 01:11:09 +0200

    fix: zip inflate is now a separate package

---
+++ packages/middlewares/binary-processors/zip-processor/src/impl/zip-deflate/lambdas/zip/index.ts
@@ -0,0 +1,183 @@
+/*
+ * Copyright (C) 2023 Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import archiver from 'archiver';
+
+import { randomUUID } from 'crypto';
+import { S3Stream, S3DocumentDescriptor } from '@project-lakechain/sdk/helpers';
+import { SQSEvent, Context, SQSRecord, SQSBatchResponse } from 'aws-lambda';
+import { logger, tracer } from '@project-lakechain/sdk/powertools';
+import { LambdaInterface } from '@aws-lambda-powertools/commons';
+import { CloudEvent, DataEnvelope, Document, EventType as Type } from '@project-lakechain/sdk/models';
+import { next } from '@project-lakechain/sdk/decorators';
+import { CompleteMultipartUploadCommandOutput } from '@aws-sdk/client-s3';
+
+import {
+  BatchProcessor,
+  EventType,
+  processPartialResponse
+} from '@aws-lambda-powertools/batch';
+
+/**
+ * Environment variables.
+ */
+const TARGET_BUCKET = process.env.PROCESSED_FILES_BUCKET as string;
+
+/**
+ * The async batch processor processes the received
+ * events from SQS in parallel.
+ */
+const processor = new BatchProcessor(EventType.SQS);
+
+/**
+ * The S3 stream helper.
+ */
+const s3Stream = new S3Stream();
+
+class Lambda implements LambdaInterface {
+
+  /**
+   * Called back when the Zip archive has been created.
+   * @param uploadResult the upload result.
+   * @param size the size of the archive.
+   */
+  @next()
+  private onArchiveCreated(
+    result: CompleteMultipartUploadCommandOutput,
+    size: number
+  ): Promise<CloudEvent> {
+    const document = new Document.Builder()
+      .withUrl(new S3DocumentDescriptor({
+          bucket: result.Bucket!,
+          key: result.Key!
+        }).asUri()
+      )
+      .withEtag(result.ETag!.replace(/"/g, ''))
+      .withSize(size)
+      .withType('application/zip')
+      .build();
+
+    return (Promise.resolve(new CloudEvent.Builder()
+      .withId(randomUUID())
+      .withType(Type.DOCUMENT_CREATED)
+      .withData(new DataEnvelope.Builder()
+        .withChainId(randomUUID())
+        .withSourceDocument(document)
+        .withDocument(document)
+        .withMetadata({})
+        .build())
+      .build()));
+  }
+
+  /**
+   * Resolves the input documents to process.
+   * This function supports both single and composite events.
+   * @param event the received event.
+   * @returns a promise to an array of CloudEvents to process.
+   */
+  private async resolveEvents(event: CloudEvent): Promise<CloudEvent[]> {
+    const document = event.data().document();
+
+    if (document.mimeType() === 'application/cloudevents+json') {
+      const data = JSON.parse((await document
+        .data()
+        .asBuffer())
+        .toString('utf-8')
+      );
+      return (data.map((event: string) => CloudEvent.from(event)));
+    } else {
+      return ([event]);
+    }
+  }
+
+  /**
+   * Processes the given event and zips the documents
+   * associated with it into the bucket passed as `destBucket`.
+   * The entire operation happens in streaming to avoid loading
+   * the entirety of the zip file in memory.
+   * @param events the events to process.
+   * @param destBucket the destination bucket where
+   * to unzip the resulting files.
+   * @returns a promise which resolves when the
+   * unzip process is complete.
+   */
+  private async unzip(events: CloudEvent[], destBucket: string) {
+    const outputKey = `${randomUUID}/archive.zip`;
+
+    // Create a new archiver instance.
+    const archive = archiver('zip', {
+      zlib: { level: 9 }
+    });
+
+    // Create a write stream to the destination bucket.
+    const { writeStream, promise } = s3Stream.createS3WriteStream({
+      bucket: destBucket,
+      key: outputKey,
+      contentType: 'application/zip'
+    });
+
+    // Pipe the archive to the write stream.
+    archive.pipe(writeStream);
+
+    // Append each document read stream to the archive.
+    for (const event of events) {
+      const document = event.data().document();
+      archive.append(
+        await document.data().asReadStream(),
+        { name: document.filename().basename() }
+      );
+    }
+
+    // Wait for the process to complete.
+    await archive.finalize();
+    const res = await promise;
+    
+    return (this.onArchiveCreated(res, archive.pointer()));
+  }
+
+  /**
+   * Inflates the document associated with the record
+   * in the target S3 bucket.
+   * @param record the SQS record associated with
+   * the document to inflate.
+   */
+  async recordHandler(record: SQSRecord): Promise<any> {
+    return (this.unzip(
+      await this.resolveEvents(CloudEvent.from(JSON.parse(record.body))),
+      TARGET_BUCKET
+    ));
+  }
+
+  /**
+   * The Lambda entry point.
+   * @param event the received SQS event.
+   * @param _ the Lambda context.
+   */
+  @tracer.captureLambdaHandler()
+  @logger.injectLambdaContext()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async handler(event: SQSEvent, _: Context): Promise<SQSBatchResponse> {
+    return (await processPartialResponse(
+      event, this.recordHandler.bind(this), processor
+    ));
+  }
+}
+
+// The Lambda handler class.
+const handlerClass = new Lambda();
+
+// The handler function.
+export const handler = handlerClass.handler.bind(handlerClass);
---
+++ packages/middlewares/binary-processors/zip-processor/src/impl/zip-inflate/index.ts
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2023 Amazon.com, Inc. or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import path from 'path';
+
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sources from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as node from 'aws-cdk-lib/aws-lambda-nodejs';
+
+import { Construct } from 'constructs';
+import { ServiceDescription } from '@project-lakechain/core/service';
+import { ComputeType } from '@project-lakechain/core/compute-type';
+import { when } from '@project-lakechain/core/dsl/vocabulary/conditions';
+import { CacheStorage } from '@project-lakechain/core';
+
+import {
+  Middleware,
+  MiddlewareBuilder,
+  MiddlewareProps,
+  LAMBDA_INSIGHTS_VERSION,
+  NAMESPACE
+} from '@project-lakechain/core/middleware';
+
+/**
+ * The service description.
+ */
+const description: ServiceDescription = {
+  name: 'zip-inflate-processor',
+  description: 'Unzips compressed files from a source to a destination bucket.',
+  version: '0.4.0',
+  attrs: {}
+};
+
+/**
+ * The maximum time the lambda function
+ * is allowed to run.
+ */
+const PROCESSING_TIMEOUT = cdk.Duration.minutes(15);
+
+/**
+ * The execution runtime for used compute.
+ */
+const EXECUTION_RUNTIME  = lambda.Runtime.NODEJS_18_X;
+
+/**
+ * The default memory size to allocate for the compute.
+ */
+const DEFAULT_MEMORY_SIZE = 512;
+
+/**
+ * The builder for the `ZipInflateProcessor` middleware.
+ */
+class ZipInflateProcessorBuilder extends MiddlewareBuilder {
+
+  /**
+   * @returns a new instance of the `ZipInflateProcessor`
+   * service constructed with the given parameters.
+   */
+  public build(): ZipInflateProcessor {
+    return (new ZipInflateProcessor(
+      this.scope,
+      this.identifier, {
+        ...this.props
+      }
+    ));
+  }
+}
+
+/**
+ * A middleware providing a way to unzip compressed
+ * archives from a source bucket into a destination
+ * bucket.
+ */
+export class ZipInflateProcessor extends Middleware {
+
+  /**
+   * The storage containing processed files.
+   */
+  public storage: CacheStorage;
+
+  /**
+   * The data processor lambda function.
+   */
+  public processor: lambda.IFunction;
+
+  /**
+   * The builder for the `ZipInflateProcessor` service.
+   */
+  static Builder = ZipInflateProcessorBuilder;
+
+  /**
+   * Construct constructor.
+   */
+  constructor(scope: Construct, id: string, props: MiddlewareProps) {
+    super(scope, id, description, {
+      ...props,
+      queueVisibilityTimeout: cdk.Duration.seconds(
+        3 * PROCESSING_TIMEOUT.toSeconds()
+      )
+    });
+
+    ///////////////////////////////////////////
+    ////////    Processing Storage      ///////
+    ///////////////////////////////////////////
+
+    this.storage = new CacheStorage(this, 'Storage', {
+      encryptionKey: props.kmsKey
+    });
+
+    ///////////////////////////////////////////
+    //////    Middleware Event Handler     ////
+    ///////////////////////////////////////////
+
+    this.processor = new node.NodejsFunction(this, 'Processor', {
+      description: 'Inflates ZIP archives from a source to a destination bucket.',
+      entry: path.resolve(__dirname, 'lambdas', 'unzip', 'index.js'),
+      vpc: props.vpc,
+      memorySize: props.maxMemorySize ?? DEFAULT_MEMORY_SIZE,
+      timeout: PROCESSING_TIMEOUT,
+      runtime: EXECUTION_RUNTIME,
+      architecture: lambda.Architecture.ARM_64,
+      tracing: lambda.Tracing.ACTIVE,
+      environmentEncryption: props.kmsKey,
+      logGroup: this.logGroup,
+      insightsVersion: props.cloudWatchInsights ?
+        LAMBDA_INSIGHTS_VERSION :
+        undefined,
+      environment: {
+        POWERTOOLS_SERVICE_NAME: description.name,
+        POWERTOOLS_METRICS_NAMESPACE: NAMESPACE,
+        PROCESSED_FILES_BUCKET: this.storage.id(),
+        SNS_TARGET_TOPIC: this.eventBus.topicArn
+      },
+      bundling: {
+        minify: true,
+        externalModules: [
+          '@aws-sdk/client-s3',
+          '@aws-sdk/client-sns',
+          '@aws-sdk/lib-storage'
+        ]
+      }
+    });
+
+    // Allows this construct to act as a `IGrantable`
+    // for other middlewares to grant the processing
+    // lambda permissions to access their resources.
+    this.grantPrincipal = this.processor.grantPrincipal;
+
+    // Plug the SQS queue into the lambda function.
+    this.processor.addEventSource(new sources.SqsEventSource(this.eventQueue, {
+      batchSize: props.batchSize ?? 1,
+      reportBatchItemFailures: true
+    }));
+
+    // Grant the lambda function permissions to
+    // write to the post-processing storage.
+    this.storage.grantWrite(this.processor);
+
+    // Grant the lambda function permissions to
+    // publish to the SNS topic.
+    this.eventBus.grantPublish(this.processor);
+
+    super.bind();
+  }
+
+  /**
+   * Allows a grantee to read from the processed documents
+   * generated by this middleware.
+   */
+  public grantReadProcessedDocuments(grantee: iam.IGrantable): iam.Grant {
+    return (this.storage.grantRead(grantee));
+  }
+
+  /**
+   * @returns an array of mime-types supported as input
+   * type by this middleware.
+   */
+  supportedInputTypes(): string[] {
+    return ([
+      'application/zip'
+    ]);
+  }
+
+  /**
+   * @returns an array of mime-types supported as output
+   * type by the data producer.
+   */
+  supportedOutputTypes(): string[] {
+    return ([
+      '*/*'
+    ]);
+  }
+
+  /**
+   * @returns the supported compute types by a given
+   * middleware.
+   */
+  supportedComputeTypes(): ComputeType[] {
+    return ([
+      ComputeType.CPU
+    ]);
+  }
+
+  /**
+   * @returns the middleware conditional statement defining
+   * in which conditions this middleware should be executed.
+   * In this case, we want the middleware to only be invoked
+   * when the document mime-type is supported, and the event
+   * type is `document-created`.
+   */
+  conditional() {
+    return (super
+      .conditional()
+      .and(when('type').equals('document-created'))
+    );
+  }
+}
